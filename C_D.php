02b7da41a2e3b7d9a710bf97d3c943f985e3993e
Eduardo Meurer <eduardomeurer999@gmail.com>
2018-08-10 23:20:17 -0300

    Trabalho finalizado

--- C_D.php
+++ C_D.php
@@ -1,44 +1,43 @@
 <?php
 
-
-$chave = $_POST['chave'];
-$tipo = $_POST['tipo'];
-$fn = $_POST['fn'];
-
-
-
+function all()
+{
+    $chave = $_POST['chave'];
+    $tipo = $_POST['tipo'];
+    $fn = $_POST['fn'];
 //Algoritmo cripta e decripta AES
-$return = "";
-$input = "";
-switch ($tipo) {
-    case "AES":
-        $method = 'aes-256-cbc';
-        for ($i = 0; $i < 16; $i++) {
-            $iv .= chr(0x0);
-        }
-        if ($fn == 'cript') {
-            $texto = $_POST['texto'];
-            $encrypted = base64_encode(openssl_encrypt($texto, $method, $chave, OPENSSL_RAW_DATA, $iv));
-            $return = $encrypted;
-            $input = "C";
-        }
-        else if ($fn == 'des'){
-            $texto = $_POST['textoC'];
-            $decrypted = openssl_decrypt(base64_decode($texto), $method, $chave, OPENSSL_RAW_DATA, $iv);
-            $return = $decrypted;
-            $input = "D";
-        }
-        else{
-            $return = "erro";
-        }
-        break;
-    case "DES":
-        break;
-    case "Blowfish":
-        break;
-    case "Twofish":
-        break;
+    $return = "";
+    $input = "";
+    switch ($tipo) {
+        case "AES":
+            $iv = "";
+            $method = 'aes-256-cbc';
+            for ($i = 0; $i < 16; $i++) {
+                $iv .= chr(0x0);
+            }
+            if ($fn == 'cript') {
+                $texto = $_POST['texto'];
+                $encrypted = base64_encode(openssl_encrypt($texto, $method, $chave, OPENSSL_RAW_DATA, $iv));
+                $return = $encrypted;
+            } else if ($fn == 'descript') {
+                $texto = $_POST['textoC'];
+                $decrypted = openssl_decrypt(base64_decode($texto), $method, $chave, OPENSSL_RAW_DATA, $iv);
+                $return = $decrypted;
+            } else {
+                $return = "erro";
+            }
+            break;
+        case "DES":
+            break;
+        case "Blowfish":
+            break;
+        case "Twofish":
+            break;
+
+    }
+    return $return;
 }
-echo 
+
+echo all();
 
 //
--- index.php
+++ index.php
@@ -5,81 +5,120 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Criptografia</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript">
-            function onClick(metodo) {
-                texto = $('#texto').val();
-                chave = $('#chave').val();
-                textoC = $('#textoCripto').val();
-                opt = "";
-                $("#opt option:selected").each(function () {
-                    opt = $(this).val();
-                });
-                data = "";
-                switch (metodo) {
-                    case "cripto":
-                        $('#texto').css('border', '1px solid black');
-                        $('#chave').css('border', '1px solid black');
-                        $('#textoCripto').css('border', '1px solid black');
-                        if ((texto !== "") && (chave !== "") && (textoC === "")) {
-                            data = "texto=" + texto + "&chave=" + chave+"&fn=cri";
-                        } else {
-                            if (texto === "")
-                                $('#texto').css('border', '1px solid red');
-                            if (chave === "")
-                                $('#chave').css('border', '1px solid red');
-                            break;
+<head>
+    <meta charset="UTF-8">
+    <title>Criptografia</title>
+    <script src="jquery-3.3.1.slim.js"></script>
+    <script type="text/javascript">
+        function onClick(metodo) {
+            texto = $('#texto').val();
+            chave = $('#chave').val();
+            textoC = $('#textoCripto').val();
+            opt = $("#opt option:selected").val();
+            switch (metodo) {
+                case
+                "cripto"
+                :
+                    if(textoC !== ""){
+                        $('#textoCripto').val("");
+                        textoC = "";
+                    }
+                    $('#texto').css('border', '1px solid black');
+                    $('#chave').css('border', '1px solid black');
+                    $('#textoCripto').css('border', '1px solid black');
+                    if ((texto !== "") && (chave !== "") && (textoC === "")) {
+                        data = "texto=" + texto + "&chave=" + chave + "&fn=cript";
+                        if (data !== "") {
+                            data += "&tipo=" + opt;
+                            $.ajax({
+                                type: "POST",
+                                url: "./C_D.php",
+                                data: data,
+                                success: function ( result ) {
+                                    $('#texto').css('border', '1px solid black');
+                                    $('#chave').css('border', '1px solid black');
+                                    $('#textoCripto').css('border', '1px solid black');
+                                    $('#textoCripto').val(result);
+
+                                },
+                                error: function (result) {
+                                    console.log(result);
+                                }
+                            });
                         }
-                    case "decripto":
-                        $('#texto').css('border', '1px solid black');
-                        $('#chave').css('border', '1px solid black');
-                        $('#textoCripto').css('border', '1px solid black');
-                        if ((texto === "") && (chave !== "") && (textoC !== "")) {
-                            data = "textoC=" + textoC + "&chave=" + chave+"&fn=des";
-                        } else {
-                            if (chave === "")
-                                $('#chave').css('border', '1px solid red');
-                            if (textoC === "")
-                                $('#textoCripto').css('border', '1px solid red');
+                        else {
+                            alert("Não foi possível enviar os textos ao servidor");
                         }
-                        break;
-                }
-                
-                if (data !== "") {
-                    data += "&tipo="+opt;
-                    $.ajax({
-                        type: "POST",
-                        url: "./C_D.php",
-                        data: data,
-                        success: function (response) {
-                            $('#texto').css('border', '1px solid black');
-                            $('#chave').css('border', '1px solid black');
-                            $('#textoCripto').css('border', '1px solid black');   
-                            alert(response);
+                    } else {
+                        if (texto === "")
+                            $('#texto').css('border', '1px solid red');
+                        if (chave === "")
+                            $('#chave').css('border', '1px solid red');
+                        if (textoC !== "")
+                            $('#textoCripto').css('border', '1px solid red');
+
+                    }
+                    data = "";
+                    break;
+                    case
+                "decripto"
+                :
+                    $('#texto').css('border', '1px solid black');
+                    $('#chave').css('border', '1px solid black');
+                    $('#textoCripto').css('border', '1px solid black');
+                    if ((texto === "") && (chave !== "") && (textoC !== "")) {
+                        data = "textoC=" + textoC + "&chave=" + chave + "&fn=descript";
+                        if (data !== "") {
+                            data += "&tipo=" + opt;
+                            $.ajax({
+                                type: "POST",
+                                url: "./C_D.php",
+                                data: data,
+                                success: function (result) {
+                                    $('#texto').css('border', '1px solid black');
+                                    $('#chave').css('border', '1px solid black');
+                                    $('#textoCripto').css('border', '1px solid black');
+
+                                    $('#texto').val(result);
+
+
+                                },
+                                error: function (result) {
+                                    console.log(result);
+                                }
+                            });
                         }
-                    });
-                } else {
-                    alert("Não foi possível enviar os textos ao servidor");
-                }
+                        else {
+                            alert("Não foi possível enviar os textos ao servidor");
+                        }
+                    } else {
+                        if (chave === "")
+                            $('#chave').css('border', '1px solid red');
+                        if (texto !== "")
+                            $('#texto').css('border', '1px solid red');
+                        if (textoC === "")
+                            $('#textoCripto').css('border', '1px solid red');
+                    }
+                    data = "";
+                    break;
             }
-        </script>
-    </head>
-    <body>
-        Texto: <textarea id="texto"></textarea><br><br>
-        Chave: <input type="text" id="chave"><br><br>
-        Texto criptografado:<br><br>
-        <textarea id="textoCripto" ></textarea>
-        <br><br>
-        <select name="opt">
-            <option value="AES">AES</option> 
-            <option value="DES" selected>DES</option>
-            <option value="Twofish">Twofish</option>
-            <option value="Blowfish">Blowfish</option>
-        </select>
-        <input type="submit" value="Criptografar" id="cripto" onclick="onClick('cripto')">
-        <input type="submit" value="Descriptografar" id="decripto" onclick="onClick('decripto')">
-    </body>
+
+        }
+    </script>
+</head>
+<body>
+Texto: <textarea id="texto"></textarea><br><br>
+Chave: <input type="text" id="chave"><br><br>
+Texto criptografado:<br><br>
+<textarea id="textoCripto"></textarea>
+<br><br>
+<select id="opt">
+    <option value="AES">AES</option>
+    <option value="DES">DES</option>
+    <option value="Twofish">Twofish</option>
+    <option value="Blowfish">Blowfish</option>
+</select>
+<input type="submit" value="Criptografar" id="cripto" onclick="onClick('cripto')">
+<input type="submit" value="Descriptografar" id="decripto" onclick="onClick('decripto')">
+</body>
 </html>
